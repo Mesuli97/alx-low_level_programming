79ab1574425cfa55f3913d90e0ec00c0ca7259f8
“Mesuli” <mesulimdladla@gmail.com>
2022-09-09 15:57:48 -0700

    suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char love;
+	
+	for (love = 'a'; love <= love <= 'z'; love++)
+		putchar(love);
+	putchar('\n');
+	return (0);
+}
