28f414421eacadfb77872a76cbd4693af1512c8d
“Mesuli” <mesulimdladla@gmail.com>
2022-09-09 17:50:03 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
