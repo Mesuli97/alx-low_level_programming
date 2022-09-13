d34d021d36a8a50c24a1541d43e5c2fc2a004f8b
“Mesuli” <mesulimdladla@gmail.com>
2022-09-13 07:58:49 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always return 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char main[] = "main";
+	int i = 0;
+
+	while (main[i] != '\0')
+	{
+		_putchar(main[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
