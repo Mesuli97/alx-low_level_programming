78ecfe06a65e9ed35fdc3d8bdf7bd68e1beb96b1
“Mesuli” <mesulimdladla@gmail.com>
2022-09-13 12:20:23 -0700

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	{
+		void print_alphabet_x10(void)
+	}
+		char i;
+		char j;
+
+		for (i = 0; i <= 9; i++)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(i);
+	}
+_putchar('\n');
+	}
+}
