f03a39c535c0808ee75b07e81f6a904aadee95ea
“Mesuli” <mesulimdladla@gmail.com>
2022-09-13 04:24:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <_putchar.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always return 0
+ */
+int main(void)
+{
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+
+	return (0);
+}
