766e258396b4d91d0da221b812b0ae7d4448ef24
“Mesuli” <mesulimdladla@gmail.com>
2022-09-15 05:46:54 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0123456789
- * Description: You can only use _putchar twice
- * Return: void
+ * print_numbers - print 0 - 9
+ *	only using _putchar twice
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	char c;
+	int num = 0;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n')
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
 }
